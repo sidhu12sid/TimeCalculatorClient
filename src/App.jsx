97ea1f7f4e1a553b3047528f1c74d516e6b3d897
Sidharth Soman <sidharthsoman065@gmail.com>
2025-04-29 15:23:45 +0530

    time calculator client

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+        <div>
+          <h1>Time Calculation</h1>
+          <p>This is a simple time calculation application.</p>
+        </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TimeCalculation-Component/timecalculator.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const timecalculator = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default timecalculator
