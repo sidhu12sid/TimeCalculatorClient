f0824779cfffc93993ade85e6ef2f7d004b4b46b
Sidharth Soman <sidharthsoman065@gmail.com>
2025-04-29 17:02:46 +0530

    new packages

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <>
-        <div>
-          <h1>Time Calculation</h1>
-          <p>This is a simple time calculation application.</p>
-        </div>
+        
     </>
   )
 }
