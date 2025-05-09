2282c65794947d58484bca96b521c6ff49361ffa
Sidharth Soman <sidharthsoman065@gmail.com>
2025-05-09 17:44:39 +0530

    configured tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const App = () => {
   return (
     <>
-        
+        <div className=' flex justify-center items-center'>
+          <div className='bg-white w-96 h-screen'>
+            <h1> hello world</h1>
+          </div>
+        </div>
     </>
   )
 }
