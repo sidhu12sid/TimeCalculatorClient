a87df6cc1ef1b427f0276cf648700adc4cea8fa5
Sidharth Soman <sidharthsoman065@gmail.com>
2025-05-12 11:06:15 +0530

    added api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const App = () => {
+const [punchData, setPunchData] = useState("");
+const [response, setResponse] = useState("");
+
+const handleSubmit =  async (e) => {
+  e.preventDefault();
+  const urlProd = "https://timecalculator-deebf4apfbhvenca.canadacentral-01.azurewebsites.net/api/Punch/calculate";
+  const urlDev ="https://localhost:44338/api/Punch/calculate";
+
+  const result = await axios.post(urlDev, {},{
+    params : { punchData },
+    headers: {
+      'accept': '*/*',
+      'Content-Type': 'application/json-patch+json'
+    }
+  });
+
+  console.log(result.data);
+};
+
   return (
     <>
       <div className=" flex justify-center items-center h-screen">
@@ -8,8 +28,9 @@ const App = () => {
           <div className="flex justify-center">
           <h1 className="text-2xl">Time calculator</h1>
           </div>
+          <form onSubmit={handleSubmit}>
           <div className="col-span-full p-6">
-            <label for="about" className="block text-lg font-medium text-gray-900">
+            <label htmlFor="about" className="block text-lg font-medium text-gray-900">
               Enter your timesheet data :
             </label>
             <div className="mt-2">
@@ -17,6 +38,8 @@ const App = () => {
                 name="about"
                 id="about"
                 rows="8"
+                value={punchData}
+                onChange={(e => setPunchData(e.target.value))}
                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
               ></textarea>
             </div>
@@ -27,8 +50,9 @@ const App = () => {
 
           <div className="mt-6 flex items-center justify-center gap-x-6">
           {/* <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button> */}
-          <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Check</button>
+          <button type="submit" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Check</button>
           </div>
+          </form>         
         </div>
 
       </div>
