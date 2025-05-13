ed894948851fd44599c88241cbe4d68510e3bf64
Sidharth Soman <sidharthsoman065@gmail.com>
2025-05-13 17:22:11 +0530

    style fixes

--- src/App.jsx
+++ src/App.jsx
@@ -75,7 +75,7 @@ const App = () => {
             <div className="mt-4 flex items-center justify-center gap-x-6">
               <button
                 type="submit"
-                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-md w-45 bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Calculate
               </button>
